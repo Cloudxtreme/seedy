ae7117d5b299adab83371603f5411d591e621216
Nick Whalen <nickw@mindstorm-networks.net>
2013-06-24 23:44:41 -0700

    Tests for find_questions_by_owner

---
+++ preseed/tests/file_test.py
@@ -0,0 +1,269 @@
+# coding=utf-8
+#
+# $Id: $
+#
+# NAME:         file_test.py
+#
+# AUTHOR:       Nick Whalen <nickw@mindstorm-networks.net>
+# COPYRIGHT:    2013 by Nick Whalen
+# LICENSE:
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# DESCRIPTION:
+#   Tests the preseed's File class
+#
+
+import pytest
+from preseed import file
+
+
+class Test___init__(object):
+    """
+    Tests the __init__ method
+
+    """
+    def setup_method(self, method):
+        """
+        Test setup
+
+        """
+        pfile = file.File()
+    #---
+
+    def test_(self):
+        """
+        Tests
+
+        """
+    #---
+
+#---
+
+class Test___getitem__(object):
+    """
+    Tests the __getitem__ method
+
+    """
+    def setup_method(self, method):
+        """
+        Test setup
+
+        """
+        pfile = file.File()
+    #---
+
+    def test_(self):
+        """
+        Tests
+
+        """
+    #---
+
+#---
+
+class Test__file_exists(object):
+    """
+    Tests the _file_exists method
+
+    """
+    def setup_method(self, method):
+        """
+        Test setup
+
+        """
+        pfile = file.File()
+    #---
+
+    def test_(self):
+        """
+        Tests
+
+        """
+    #---
+
+#---
+
+class Test_to_text(object):
+    """
+    Tests the to_text method
+
+    """
+    def setup_method(self, method):
+        """
+        Test setup
+
+        """
+        pfile = file.File()
+    #---
+
+    def test_(self):
+        """
+        Tests
+
+        """
+    #---
+
+#---
+
+class Test_load(object):
+    """
+    Tests the load method
+
+    """
+    def setup_method(self, method):
+        """
+        Test setup
+
+        """
+        pfile = file.File()
+    #---
+
+    def test_(self):
+        """
+        Tests
+
+        """
+    #---
+
+#---
+
+class Test_save(object):
+    """
+    Tests the save method
+
+    """
+    def setup_method(self, method):
+        """
+        Test setup
+
+        """
+        pfile = file.File()
+    #---
+
+    def test_(self):
+        """
+        Tests
+
+        """
+    #---
+
+#---
+
+class Test_find_questions_by_owner(object):
+    """
+    Tests the find_questions_by_owner method
+
+    """
+    def setup_method(self, method):
+        """
+        Test setup
+
+        """
+        self.preseed_data = {
+            'd-i': {
+                'pkgsel/include': ['string', 'vim openssh-server python-pip python-software-properties salt-minion rng-tools'],
+                'pkgsel/language-packs': ['multiselect', None],
+                'pkgsel/update-policy': ['select', 'none'],
+                'pkgsel/updatedb': ['boolean', 'true'],
+                'pkgsel/upgrade': ['select', 'safe-upgrade'],
+                'partman-lvm/confirm': ['boolean', 'true'],
+            }
+        }
+        self.filtered_dict = {
+            'd-i': {
+                'pkgsel/include': ['string', 'vim openssh-server python-pip python-software-properties salt-minion rng-tools'],
+                'pkgsel/language-packs': ['multiselect', None],
+                'pkgsel/update-policy': ['select', 'none'],
+                'pkgsel/updatedb': ['boolean', 'true'],
+                'pkgsel/upgrade': ['select', 'safe-upgrade'],
+            }
+        }
+        self.filtered_list = ['pkgsel/include', 'pkgsel/language-packs', 'pkgsel/update-policy', 'pkgsel/updatedb', 'pkgsel/upgrade',]
+
+        self.pfile = file.File()
+        self.pfile._data = self.preseed_data.copy()
+    #---
+
+    def test_ReturnsEmptyDictForNoMatches(self):
+        """
+        Tests that the method returns an empty dict when no matches are found.
+
+        """
+        questions = self.pfile.find_questions_by_owner('d-i', 'bob')
+        assert questions == {}
+    #---
+
+    def test_ReturnsEmptyListForNoMatches_WithIncludeDataFalse(self):
+        """
+        Tests that the method returns an empty list if no matches are found and include_data is set to ``False``.
+
+        """
+        questions = self.pfile.find_questions_by_owner('d-i', 'bob', with_data=False)
+        assert questions == []
+    #---
+
+    def test_ReturnsPopulatedDictIfMatches(self):
+        """
+        Tests that the method will return a dict in the following format for questions that match the criteria:
+            {
+                'owner': {
+                    'question': ['type', 'value']
+                }
+            }
+
+        """
+        questions = self.pfile.find_questions_by_owner('d-i', 'pkgsel')
+        assert questions == self.filtered_dict['d-i']
+    #---
+
+    def test_ReturnsPopulatedListIfMatches_WithIncludeDataFalse(self):
+        """
+        Tests that the method will return a list of only question names if there are matches and include_data is `
+        `False``.
+
+        """
+        questions = self.pfile.find_questions_by_owner('d-i', 'pkgsel', with_data=False)
+        assert sorted(questions) == sorted(self.filtered_list)
+    #---
+
+    def test_RaisesOwnerErrorForNonExistentOwners(self):
+        """
+        Tests that the method will raise an error for owners that do not exist.
+
+        """
+        with pytest.raises(file.OwnerError):
+            self.pfile.find_questions_by_owner('bob', '')
+    #---
+#---
+
+class Test_find_questions(object):
+    """
+    Tests the find_questions method
+
+    """
+    def setup_method(self, method):
+        """
+        Test setup
+
+        """
+        pfile = file.File()
+    #---
+
+    def test_(self):
+        """
+        Tests
+
+        """
+    #---
+
+#---
